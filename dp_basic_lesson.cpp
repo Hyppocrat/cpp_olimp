a8c213f648ff5a0c087356e9f862effa08c29eb7
Hyppocrat <djsh92@mail.ru>
2019-11-08 20:45:38 +0300

    Update dp_basic_lesson.cpp

--- dp_basic_lesson.cpp
+++ dp_basic_lesson.cpp
@@ -109,3 +109,110 @@ int main(){
     }*/
     
 }
+
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+/*char ch[35];
+int a[35], c[300];
+bool b[35]; 
+string s;
+
+void rec(int k, int n){
+    if (k == n){
+        for(int i = 0; i < n; i++){
+            cout << ch[i]; 
+        }
+        cout << endl;
+    }
+    else {
+        for(int i = 0; i < 256; i++){
+            if (c[i] > 0){
+                ch[k] = i;
+                c[i]--;
+                rec(k + 1, n);
+                c[i]++;
+            }
+        }
+    }
+}
+    int n;
+    cin >> s;
+    //sort(s.begin(), s.end()); // сортировка символов строки
+    n = s.size();
+    memset(b, 0, sizeof(b));
+    memset(c, 0, sizeof(c));
+    for(int i = 0; i < n; i++){
+        c[s[i]]++;
+    }
+    rec(0, n);*/
+// поле n * m, в левом верхнем углу - черепаха
+// черепаха может ползти либо в соседнюю справа клетку,
+// либо в соседнюю снизу клетку
+// в каждой клетке есть число - штраф, который
+// черепаха платит, переходя в эту клетку
+// добраться до нижнего правого угла, собрав минимальное 
+// количество штрафа
+// n, m <= 100
+// a[i][j] <= 1000000
+// (n + m) * 1000000
+// inf inf inf inf inf inf inf
+// inf 1 5 1 5 inf
+// inf 2 1 6 1 inf
+// inf 4 1 2 1 inf
+// inf inf inf inf inf inf inf
+
+
+int main(){
+    freopen("input.txt", "rt", stdin);
+    freopen("output.txt", "wt", stdout);
+    int n, m, a[113][113], dp[113][113];
+    cin >> n >> m;
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            cin >> a[i][j];
+        }
+    }
+    for(int i = 0; i <= m + 1; i++){
+        dp[0][i] = dp[n + 1][i] = (n + m) * 1000000;
+    }
+    for(int i = 1; i <= n; i++){
+        dp[i][0] = dp[i][m + 1] = (n + m) * 1000000;
+    }
+    dp[1][1] = a[1][1];
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if ((i != 1) || (j != 1)){
+                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + a[i][j];
+            }
+        }
+    }
+    cout << dp[n][m] << endl;
+    int i = n, j = m;
+    //cout << i << " " << j << endl;
+    char ch[113][113];
+    memset(ch, '#', sizeof(ch));
+    ch[i][j] = '+';
+    while (i != 1 || j != 1){
+        if (dp[i - 1][j] > dp[i][j - 1]){
+            j--;
+            //cout << i << " " << j << endl;
+            ch[i][j] = '+';
+        }
+        else {
+            i--;
+            ch[i][j] = '+';
+            //cout << i << " " << j << endl;
+        }
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            cout << ch[i][j];
+        }
+        cout << endl;
+    }
+}
+
